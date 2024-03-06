2406cb5312a7c4862c8eee94774c0a7c3ca33254
Gingerberd <157032953+Gingerberd@users.noreply.github.com>
2024-03-06 14:53:22 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+import csv
+with open('FIOandTelephone.csv', 'r', encoding='utf-8') as f:
+    reader = csv.DictReader(f)
+    fio = input("ФИО: ")
+    for obj['\ufeffFIO'] == fio:
+        print(obj["Telephone"])
